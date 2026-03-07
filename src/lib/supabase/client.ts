1200bc134618c65feb55d292cc441bd053c50ee5
David Cho <csi00700@gmail.com>
2026-03-08 01:43:27 +0900

    Commit remaining root workspace changes

--- src/lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { resolveSupabaseEnv } from "./env";
-
-export function createBrowserSupabaseClient() {
-  const { url, anonymousKey } = resolveSupabaseEnv();
-  return createBrowserClient(url, anonymousKey);
-}
